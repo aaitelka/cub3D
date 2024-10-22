6c565c5e23212da62763eb52e96adcedc2d8b767
aaitelka <aaitelka@student.1337.ma>
2024-10-22 20:53:10 +0100

    leak fixed when color has only commas

--- src/parse/ft_array_utils.c
+++ src/parse/ft_array_utils.c
@@ -6,17 +6,12 @@
 /*   By: aaitelka <aaitelka@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 11:31:27 by aaitelka          #+#    #+#             */
-/*   Updated: 2024/10/17 13:27:47 by aaitelka         ###   ########.fr       */
+/*   Updated: 2024/10/22 20:52:00 by aaitelka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
-/**
- * @brief Get the size of the array
- * @param array The array to get the size of
- * @return The size of the array
- */
 size_t	ft_array_size(char **array)
 {
 	size_t			size;
@@ -29,17 +24,12 @@ size_t	ft_array_size(char **array)
 	return (size);
 }
 
-/**
- * @brief Clear the array of strings
- * @param array The array to clear
- * @param size The size of the array
-*/
 void	ft_clear_array(char **array, int size)
 {
 	int				index;
 
 	index = 0;
-	if (!array || !array[index] || size == 0)
+	if (!array)
 		return ;
 	while (index < size)
 	{
